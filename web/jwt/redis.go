d634d17b34f109437cf09574dde8a32cb030f8ab
LinSrHang <1023948565@qq.com>
2025-10-25 15:20:12 +0800

    fix(jwt): 修正清除token时重复设置相同header的问题
    将重复设置的登录token header改为设置刷新token header，确保正确清除所有相关token

--- web/jwt/redis.go
+++ web/jwt/redis.go
@@ -51,7 +51,7 @@ func (h *RedisJWTHandler) CheckSession(ctx *gin.Context, ssid string) error {
 
 func (h *RedisJWTHandler) ClearToken(ctx *gin.Context) error {
 	ctx.Header(constants.HeaderLoginTokenKey, "")
-	ctx.Header(constants.HeaderLoginTokenKey, "")
+	ctx.Header(constants.HeaderRefreshTokenKey, "")
 	uc := ctx.MustGet(constants.ContextUserClaimsKey).(UserClaims)
 	return h.client.Set(ctx, fmt.Sprintf(ssidKey, uc.Ssid), "", h.refreshExpiration).Err()
 }
